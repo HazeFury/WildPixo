2eb4ca27559fe73d715fdccd4e6072e1dafaeb76
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2024-04-06 18:43:12 +0200

    finish navbar component with responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,89 +1,11 @@
 import "./App.css";
 import "nes.css/css/nes.min.css";
-import Logo from "./assets/images/logo-small.png";
-import Logo2 from "./assets/images/logo.png";
+import Navbar from "./components/app-components/Navbar/Navbar";
 
 function App() {
   return (
     <main>
-      <img src={Logo} alt="logo" />
-      <img src={Logo2} alt="logo" />
-      {/*  <div>
-        <button type="button" className="nes-btn is-primary">
-          Primary
-        </button>
-        <button type="button" className="nes-btn is-success">
-          Success
-        </button>
-        <button type="button" className="nes-btn is-warning">
-          Warning
-        </button>
-        <button type="button" className="nes-btn is-error">
-          Error
-        </button>
-        <button type="button" className="nes-btn is-disabled">
-          Disabled
-        </button>
-      </div>
-      <div className="nes-field is-inline">
-        <label for="inline_field">.input.is-success</label>
-        <input
-          type="text"
-          id="inline_field"
-          className="nes-input is-success"
-          placeholder="NES.css"
-        />
-      </div>
-      <label for="textarea_field">Textarea</label>
-      <textarea id="textarea_field" className="nes-textarea"></textarea>
-      <label for="warning_select">nes-select.is-warning</label>
-      <div className="nes-container is-rounded is-dark">
-        <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
-      </div>
-      <div className="nes-select is-warning">
-        <select required id="warning_select">
-          <option value="" disabled selected hidden>
-            Select...
-          </option>
-          <option value="0">To be</option>
-          <option value="1">Not to be</option>
-        </select>
-      </div>
-      <section>
-        <button
-          type="button"
-          className="nes-btn is-primary"
-          onClick={handleChange}
-        >
-          Open dialog
-        </button>
-        {test && (
-          <form method="dialog">
-            <p className="title">Dialog</p>
-            <p>Alert: this is a dialog.</p>
-            <menu className="dialog-menu">
-              <button className="nes-btn">Cancel</button>
-              <button className="nes-btn is-primary">Confirm</button>
-            </menu>
-          </form>
-        )}
-      </section>
-      <progress
-        className="nes-progress is-primary"
-        value="80"
-        max="100"
-      ></progress>
-      <div>
-        <label>
-          <input type="radio" className="nes-radio" name="answer" checked />
-          <span>Yes</span>
-        </label>
-
-        <label>
-          <input type="radio" className="nes-radio" name="answer" />
-          <span>No</span>
-        </label>
-      </div> */}
+      <Navbar />
     </main>
   );
 }
---
+++ client/src/components/app-components/Navbar/Navbar.jsx
@@ -0,0 +1,135 @@
+import { Link, NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
+import styles from "./Navbar.module.css";
+import useScreenSize from "../../../hooks/useScreenSize";
+import Logo from "../../../assets/images/logo-small.png";
+import Menu from "../../../assets/icons/menu.svg";
+import Close from "../../../assets/icons/close.svg";
+import User from "../../../assets/icons/users.png";
+
+function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
+  const screenSize = useScreenSize();
+  const onMobileFormat = screenSize <= 1124;
+  const onDesktopFormat = screenSize > 1124;
+
+  const handleOpenMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  const handleCloseMenu = () => {
+    setOpenMenu(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleCloseMenu);
+    window.addEventListener("scroll", handleCloseMenu);
+    return () => {
+      window.removeEventListener("resize", handleCloseMenu);
+    };
+  }, []);
+  return (
+    <>
+      <nav className={styles.nav_container}>
+        <Link className={styles.navlink_style} to="/">
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <img src={Logo} alt="Wild Pixo Logo" />
+            {screenSize > 600 && <h1 style={{ marginLeft: 15 }}>Wild Pixo</h1>}
+          </div>
+        </Link>
+        {onMobileFormat && (
+          <button type="button" className="nes-btn" onClick={handleOpenMenu}>
+            <img
+              className={styles.nav_img}
+              src={openMenu ? Close : Menu}
+              alt="menu-icon"
+            />
+          </button>
+        )}
+        {onDesktopFormat && (
+          <div className={styles.nav_link_container}>
+            <NavLink className={styles.navlink_style} to="/">
+              <span className="nes-text is-primary">Accueil</span>
+            </NavLink>
+            <NavLink className={styles.navlink_style} to="/">
+              <span className="nes-text is-success">Jeux</span>
+            </NavLink>
+            <NavLink className={styles.navlink_style} to="/">
+              <span className="nes-text is-warning">Actualités</span>
+            </NavLink>
+            <NavLink className={styles.navlink_style} to="/">
+              <span className="nes-text is-error">À propos</span>
+            </NavLink>
+            <NavLink className={styles.navlink_style} to="/">
+              <button type="button" className="nes-btn">
+                <img
+                  className={styles.nav_user_btn}
+                  src={User}
+                  alt="user-logo"
+                />
+              </button>
+            </NavLink>
+          </div>
+        )}
+      </nav>
+      {openMenu && onMobileFormat && (
+        <section className={styles.opened_menu_section}>
+          <button
+            type="button"
+            className="nes-btn is-primary"
+            onClick={handleCloseMenu}
+          >
+            Accueil
+          </button>
+
+          <Link to="/">
+            <button
+              type="button"
+              className="nes-btn is-success"
+              onClick={handleCloseMenu}
+            >
+              Jeux
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              type="button"
+              className="nes-btn is-warning"
+              onClick={handleCloseMenu}
+            >
+              Actualités
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              type="button"
+              className="nes-btn is-error"
+              onClick={handleCloseMenu}
+            >
+              À propos
+            </button>
+          </Link>
+          <div style={{ marginTop: 30, borderTop: "solid white 2px" }}>
+            <Link to="/">
+              <button
+                type="button"
+                className="nes-btn"
+                onClick={handleCloseMenu}
+              >
+                Se connecter
+              </button>
+            </Link>
+          </div>
+        </section>
+      )}
+    </>
+  );
+}
+
+export default Navbar;
