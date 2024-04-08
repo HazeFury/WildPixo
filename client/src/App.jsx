ae392ad865b2e6172cadea068c645b5bd91baa47
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2024-04-08 23:42:49 +0200

    up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,30 +3,13 @@ import "nes.css/css/nes.min.css";
 import "./contrib/nesOverride.css";
 import Navbar from "./components/app-components/Navbar/Navbar";
 import Footer from "./components/app-components/Footer/Footer";
+import Header from "./components/app-components/Header/Header";
 
 function App() {
   return (
     <main>
       <Navbar />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, commodi
-        dolorum eaque officia est ratione, quos amet non vitae, nesciunt totam
-        iste quisquam et sed voluptate illo. Nostrum ab numquam nulla earum
-        laudantium suscipit animi facilis totam repudiandae fuga? Expedita amet
-        consequuntur mollitia perferendis facere dicta harum est numquam
-        aspernatur quidem delectus perspiciatis suscipit autem laborum itaque,
-        ut, libero quae beatae id ipsa aut fugit odio! Enim, laborum quasi
-        aperiam hic laudantium voluptatem soluta ducimus ratione cumque ut
-        expedita autem quaerat dignissimos tempore. Ipsa provident enim
-        perspiciatis consectetur iste voluptas sit suscipit aliquam voluptate
-        quae quas, qui sapiente delectus fuga! Sapiente magnam iusto, esse
-        perferendis corrupti qui beatae, corporis accusamus, at magni ullam.
-        Blanditiis ad sunt illum adipisci quibusdam illo, repudiandae molestiae
-        similique pariatur repellendus harum laborum eum voluptates cumque
-        necessitatibus repellat labore at tenetur voluptate ipsa ab doloremque
-        ullam ea. Molestias ipsum provident, aspernatur dicta quos qui aut.
-        Rerum.
-      </p>
+      <Header />
       <Footer />
     </main>
   );
