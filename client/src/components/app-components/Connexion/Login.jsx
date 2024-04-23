2442ed0fa98ca5062a34e09a2aab0d2fea952c56
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2024-04-24 00:22:23 +0200

    made the login component works with Context

--- client/src/components/app-components/Connexion/Login.jsx
+++ client/src/components/app-components/Connexion/Login.jsx
@@ -1,18 +1,58 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUserContext } from "../../../contexts/UserContext";
 import styles from "./Connexion.module.css";
 
 function Login() {
+  const navigate = useNavigate();
+  const { setCurrentUser, userDatabase } = useUserContext();
+  const [loginInfos, setLoginInfos] = useState({
+    pseudo: "",
+    password: "",
+  });
+
+  const handleLoginInfos = (e) => {
+    setLoginInfos({ ...loginInfos, [e.target.name]: e.target.value });
+  };
+
+  const handleLogin = () => {
+    if (
+      userDatabase.registerForm.pseudo === loginInfos.pseudo &&
+      userDatabase.registerForm.password === loginInfos.password
+    ) {
+      setCurrentUser(userDatabase.registerForm);
+      navigate("/");
+    }
+  };
+
   return (
     <div className={styles.login_container}>
       <div className="nes-field">
         <label htmlFor="pseudo">Pseudo</label>
-        <input type="text" id="pseudo" className="nes-input" />
+        <input
+          type="text"
+          name="pseudo"
+          className="nes-input"
+          value={loginInfos.pseudo}
+          onChange={handleLoginInfos}
+        />
       </div>
       <div className="nes-field">
         <label htmlFor="password">Mot de passe</label>
-        <input type="text" id="password" className="nes-input" />
+        <input
+          type="password"
+          name="password"
+          className="nes-input"
+          value={loginInfos.password}
+          onChange={handleLoginInfos}
+        />
       </div>
       <div className={styles.btn_box}>
-        <button type="button" className="nes-btn is-green">
+        <button
+          type="button"
+          className="nes-btn is-green"
+          onClick={handleLogin}
+        >
           Se connecter
         </button>
       </div>
