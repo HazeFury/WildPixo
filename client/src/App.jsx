bda2d971a06716e3a4164eabcb1467b6e7e0c663
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2024-04-23 23:18:14 +0200

    created UserContext and call it in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,17 @@ import "./contrib/nesOverride.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/app-components/Navbar/Navbar";
 import Footer from "./components/app-components/Footer/Footer";
+import { UserProvider } from "./contexts/UserContext";
 
 function App() {
   return (
-    <main>
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </main>
+    <UserProvider>
+      <main>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </main>
+    </UserProvider>
   );
 }
 
