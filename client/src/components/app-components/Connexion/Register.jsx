5b310e632cea5bacdd5b5421fab5d480e4550f3c
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2024-04-24 00:01:29 +0200

    made the register component works with Context

--- client/src/components/app-components/Connexion/Register.jsx
+++ client/src/components/app-components/Connexion/Register.jsx
@@ -1,31 +1,80 @@
+import { useState } from "react";
+import { useUserContext } from "../../../contexts/UserContext";
 import styles from "./Connexion.module.css";
 
 function Register() {
+  const { userDatabase, setUserDatabase } = useUserContext();
+  const [registerForm, setRegisterForm] = useState({
+    pseudo: "",
+    mail: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleRegiserForm = (e) => {
+    setRegisterForm({ ...registerForm, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    if (registerForm.password === registerForm.confirmPassword) {
+      setUserDatabase({ ...userDatabase, registerForm });
+    }
+  };
+
   return (
-    <div className={styles.login_container}>
+    <form className={styles.login_container}>
       <div className="nes-field">
         <label htmlFor="pseudo">Pseudo</label>
-        <input type="text" id="pseudo" className="nes-input" />
+        <input
+          type="text"
+          name="pseudo"
+          className="nes-input"
+          value={registerForm.pseudo}
+          onChange={handleRegiserForm}
+        />
       </div>
       <div className="nes-field">
         <label htmlFor="mail">Adresse mail</label>
-        <input type="text" id="mail" className="nes-input" />
+        <input
+          type="text"
+          name="mail"
+          className="nes-input"
+          value={registerForm.mail}
+          onChange={handleRegiserForm}
+        />
       </div>
 
       <div className="nes-field">
         <label htmlFor="password">Mot de passe</label>
-        <input type="text" id="password" className="nes-input" />
+        <input
+          type="password"
+          name="password"
+          className="nes-input"
+          value={registerForm.password}
+          onChange={handleRegiserForm}
+        />
       </div>
       <div className="nes-field">
-        <label htmlFor="password">Confirmez le mot de passe</label>
-        <input type="text" id="password" className="nes-input" />
+        <label htmlFor="confirmPassword">Confirmez le mot de passe</label>
+        <input
+          type="password"
+          name="confirmPassword"
+          className="nes-input"
+          value={registerForm.confirmPassword}
+          onChange={handleRegiserForm}
+        />
       </div>
       <div className={styles.btn_box}>
-        <button type="button" className="nes-btn is-blue">
+        <button
+          type="submit"
+          className="nes-btn is-blue"
+          onClick={handleSubmitForm}
+        >
           S'enregistrer
         </button>
       </div>
-    </div>
+    </form>
   );
 }
 
