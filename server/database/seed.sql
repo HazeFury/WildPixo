41662d8008e8590274f122165615f3f6fcf95e42
HazeFury <mab.dev69@gmail.com>
2024-06-03 17:19:00 +0200

    add the script for create all tables and an other for seed the DB

---
+++ server/database/seed.sql
@@ -0,0 +1,105 @@
+-- Insertion des jeux
+INSERT INTO game (name, slug, release_date, publisher, picture, description)
+ VALUES
+ ('Grand Theft Auto V', 'grand-theft-auto-v', '2013-09-17', 'Rockstar Games', "https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg", "Rockstar Games went bigger, since their previous installment of the series. You get the complicated and realistic world-building from Liberty City of GTA4 in the setting of lively and diverse Los Santos, from an old fan favorite GTA San Andreas. 561 different vehicles (including every transport you can operate) and the amount is rising with every update. \nSimultaneous storytelling from three unique perspectives: \nFollow Michael, ex-criminal living his life of leisure away from the past, Franklin, a kid that seeks the better future, and Trevor, the exact past Michael is trying to run away from. \nGTA Online will provide a lot of additional challenge even for the experienced players, coming fresh from the story mode. Now you will have other players around that can help you just as likely as ruin your mission. Every GTA mechanic up to date can be experienced by players through the unique customizable character, and community content paired with the leveling system tends to keep everyone busy and engaged.\n\nEspañol\nRockstar Games se hizo más grande desde su entrega anterior de la serie. Obtienes la construcción del mundo complicada y realista de Liberty City de GTA4 en el escenario de Los Santos, un viejo favorito de los fans, GTA San Andreas. 561 vehículos diferentes (incluidos todos los transportes que puede operar) y la cantidad aumenta con cada actualización.\nNarración simultánea desde tres perspectivas únicas:\nSigue a Michael, ex-criminal que vive su vida de ocio lejos del pasado, Franklin, un niño que busca un futuro mejor, y Trevor, el pasado exacto del que Michael está tratando de huir.\nGTA Online proporcionará muchos desafíos adicionales incluso para los jugadores experimentados, recién llegados del modo historia. Ahora tendrás otros jugadores cerca que pueden ayudarte con la misma probabilidad que arruinar tu misión. Los jugadores pueden experimentar todas las mecánicas de GTA actualizadas a través del personaje personalizable único, y el contenido de la comunidad combinado con el sistema de nivelación tiende a mantener a todos ocupados y comprometidos."),
+ ('The Witcher 3: Wild Hunt', 'the-witcher-3-wild-hunt', '2015-05-18', 'CD PROJEKT RED',  'Marigny-https://media.rawg.io/media/games/618/618c2031a07bbff6b4f611f10b6bcdbc.jpg-Châtel', "The third game in a series, it holds nothing back from the player. Open world adventures of the renowned monster slayer Geralt of Rivia are now even on a larger scale. Following the source material more accurately, this time Geralt is trying to find the child of the prophecy, Ciri while making a quick coin from various contracts on the side. Great attention to the world building above all creates an immersive story, where your decisions will shape the world around you.\n\nCD Project Red are infamous for the amount of work they put into their games, and it shows, because aside from classic third-person action RPG base game they provided 2 massive DLCs with unique questlines and 16 smaller DLCs, containing extra quests and items.\n\nPlayers praise the game for its atmosphere and a wide open world that finds the balance between fantasy elements and realistic and believable mechanics, and the game deserved numerous awards for every aspect of the game, from music to direction."),
+ ('Portal 2', 'portal-2', '2011-04-18', 'Valve Software', 'https://media.rawg.io/media/games/2ba/2bac0e87cf45e5b508f227d281c9252a.jpg', "Portal 2 is a first-person puzzle game developed by Valve Corporation and released on April 19, 2011 on Steam, PS3 and Xbox 360. It was published by Valve Corporation in digital form and by Electronic Arts in physical form. \n\nIts plot directly follows the first game's, taking place in the Half-Life universe. You play as Chell, a test subject in a research facility formerly ran by the company Aperture Science, but taken over by an evil AI that turned upon its creators, GladOS. After defeating GladOS at the end of the first game but failing to escape the facility, Chell is woken up from a stasis chamber by an AI personality core, Wheatley, as the unkempt complex is falling apart. As the two attempt to navigate through the ruins and escape, they stumble upon GladOS, and accidentally re-activate her...\n\nPortal 2's core mechanics are very similar to the first game's ; the player must make their way through several test chambers which involve puzzles. For this purpose, they possess a Portal Gun, a weapon capable of creating teleportation portals on white surfaces. This seemingly simple mechanic and its subtleties coupled with the many different puzzle elements that can appear in puzzles allows the game to be easy to start playing, yet still feature profound gameplay. The sequel adds several new puzzle elements, such as gel that can render surfaces bouncy or allow you to accelerate when running on them.\n\nThe game is often praised for its gameplay, its memorable dialogue and writing and its aesthetic. Both games in the series are responsible for inspiring most puzzle games succeeding them, particularly first-person puzzle games. The series, its characters and even its items such as the portal gun and the companion cube have become a cultural icon within gaming communities.\n\nPortal 2 also features a co-op mode where two players take on the roles of robots being led through tests by GladOS, as well as an in-depth level editor."),
+ ('Counter-Strike: Global Offensive', 'counter-strike-global-offensive', '2012-08-21', 'Valve Software', "https://media.rawg.io/media/games/736/73619bd336c894d6941d926bfd563946.jpg", 'Counter-Strike is a multiplayer phenomenon in its simplicity. No complicated narratives to explain the source of the conflict, no futuristic and alien threats, but counter-terrorists against terrorists. Arena shooter at its core, CS:GO provides you with various methods of disposing enemies and reliant on cooperation within the team. During the round of the classical clash mode, you will gradually receive currency to purchase different equipment. Each player can carry a primary weapon, secondary pistol, knife and a set of grenades, which all can change the battle if wielded by the skilled player. \r\nObjectives are clear and vary from map to map, from game mode to game mode. Stop the terrorists from planting explosives, stop the counter-terrorist from retrieving the hostages, kill everyone who isn’t you and just perform the best with.\r\nCS:GO is one of the major cybersport discipline, which makes playing it more exciting to some players. Aside from purchasing the game, CS:GO is infamous for its loot case system, that requires players to purchase keys, in order to open said cases. Customization items consist of weapon skins, weapon stickers, and sprays that do not affect gameplay and have purely visual value to the player.'),
+ ("Tomb Raider", "tomb-raider", "2013-03-05", "Crystal Dynamics", "https://media.rawg.io/media/games/021/021c4e21a1824d2526f925eff6324653.jpg", "A cinematic revival of the series in its action third person form, Tomb Rider follows Lara in her least experience period of life – her youth. Heavily influenced by Naughty Dog’s “Uncharted”, the game is a mix of everything, from stealth and survival to combat and QTE action scenes.\r\nYoung Lara Croft arrives on the Yamatai, lost island near Japan, as the leader of the expedition in search of the Yamatai Kingdom, with a diverse team of specialists. But shipwreck postponed the successful arrival and seemingly forgotten island is heavily populated with hostile inhabitants, cultists of Solarii Brotherhood.\r\nThe game will be graphic at times, especially after failed QTE’s during some of the survival scenes, but overall players will enjoy classic action adventure, reminiscent of the beginning of the series. This game is not a direct sequel or continuation of existing sub-series within the franchise, but a reboot, setting up Tomb Raider to represent modern gaming experience.\r\nThe game has RPG elements and has a world, which you can explore during the story campaign and after the completion. As well as multiplayer mode, where 2 teams (4 scavengers and 4 survivors) are clashing in 3 game modes while using weapons and environments from the single-player campaign."),
+ ("DOOM", "doom", "2016-05-13", "Bethesda Softworks", "https://media.rawg.io/media/games/587/587588c64afbff80e6f444eb2e46f9da.jpg", "Return of the classic FPS, Doom (2016) acts as a reboot of the series and brings back the Doomslayer, protagonist of the original Doom games. In order to solve the energy crisis, humanity learned to harvest the energy from Hell, and when something went wrong and a demon invasion has started, it’s up to the player to control the Doomslayer and destroy the evil.\n\nDoom is a fast-paced game that restores the concept of instant health packs and leaves the player against armies of Hell with no cover, no health regeneration, or help from anyone. After damaging monsters enough, they will start glowing, which will allow players to perform glory kills to restore some health. While exploring the levels, players will come across secrets, collectible items, or upgrade points for the weapons and armor. The single-player campaign silent protagonist has a noticeable personality. He gets visibly annoyed and angry in his actions during expository cutscenes and forces his way through the game. Multiplayer maps gather players in Deathmatch/”king-of-the-hill” type game modes, with all the weapons from the single-player campaign.");
+ 
+
+-- Insertion des actualités
+INSERT INTO news (title, intro, content, date, game_id)
+VALUES 
+('Devlog #1', 
+ 'Cette semaine, nous avons travaillé sur les mécanismes de base du jeu et sur l''implémentation du moteur physique.', 
+ 'Cette semaine, l''équipe de développement s''est concentrée sur l''intégration des mécanismes de base du jeu. Nous avons commencé par implémenter le moteur physique qui gère les interactions entre les objets du jeu. Les premiers tests sont prometteurs et montrent que notre approche permet des mouvements fluides et réalistes. En parallèle, nous avons commencé à créer les premières ébauches des niveaux de jeu, en nous assurant qu''ils soient à la fois intéressants et équilibrés pour le joueur.', 
+ '20/05/2024', 
+ NULL),
+('Devlog #2', 
+ 'Nous avons fait des progrès significatifs sur le système de combat et ajouté de nouvelles animations.', 
+ 'Cette semaine, notre focus principal a été le système de combat. Nous avons ajouté plusieurs nouvelles fonctionnalités, y compris des combos et des attaques spéciales,  qui rendent les combats beaucoup plus dynamiques et excitants. De plus, notre équipe d animation a créé de nouvelles animations pour les personnages, ce qui ajoute une couche supplémentaire de réalisme et de fluidité aux actions de combat.', 
+ '27/05/2024', 
+ NULL),
+('Devlog #3', 
+ 'L''équipe a travaillé dur sur les environnements du jeu et a commencé à implémenter les effets visuels.', 
+ 'Nous avons consacré cette semaine à l''amélioration des environnements du jeu. Nos artistes ont travaillé sur des textures et des modèles détaillés pour rendre chaque niveau unique et immersif. Parallèlement, l''équipe technique a commencé à implémenter des effets visuels tels que les éclairs et les ombres dynamiques, qui ajoutent une profondeur visuelle au jeu. Nous avons également optimisé le moteur graphique pour assurer une performance fluide sur diverses configurations matérielles.', 
+ '03/06/2024', 
+ NULL),
+('Devlog #4', 
+ 'Nous avons amélioré l''intelligence artificielle des ennemis et ajouté de nouvelles fonctionnalités de gameplay.', 
+ 'Cette semaine, l''accent a été mis sur l''amélioration de l''intelligence artificielle (IA) des ennemis. Les ennemis sont maintenant capables de réagir de manière plus stratégique aux actions du joueur, ce qui rend les combats plus engageants et moins prévisibles. De plus, nous avons ajouté plusieurs nouvelles fonctionnalités de gameplay, telles que des quêtes secondaires et des mini-jeux, qui offrent plus de diversité et de profondeur à l''expérience de jeu.',
+'10/06/2024',
+NULL),
+('Devlog #5',
+'Nous avons finalisé le système de progression des personnages et effectué des tests de jeu approfondis.',
+'Cette semaine marque une étape importante avec la finalisation du système de progression des personnages. Les joueurs peuvent maintenant gagner de l''expérience, débloquer de nouvelles compétences et améliorer leurs attributs au fil du jeu. Nous avons également commencé une série de tests de jeu approfondis pour identifier et corriger les bugs, améliorer l''équilibrage du gameplay et assurer une expérience utilisateur optimale. Les premiers retours sont très positifs et nous sommes excités par les progrès réalisés.',
+'17/06/2024',
+NULL),
+('Devlog #6',
+'Cette semaine, nous avons implémenté le système de météo dynamique et travaillé sur l''interface utilisateur.',
+'Nous avons consacré cette semaine à l''implémentation d''un système de météo dynamique. Désormais, les conditions météorologiques dans le jeu changeront en temps réel, passant du soleil à la pluie, et même à des tempêtes de neige. Cela ajoute une nouvelle dimension d''immersion pour les joueurs. En parallèle, notre équipe de design a travaillé sur l''interface utilisateur (UI), en rendant les menus plus intuitifs et visuellement attrayants.',
+'24/06/2024',
+NULL),
+('Devlog #7',
+'Nous avons ajouté de nouvelles quêtes et amélioré la narration pour enrichir l''expérience du joueur.',
+'Cette semaine a été marquée par l''ajout de nouvelles quêtes dans le jeu. Ces quêtes apportent plus de variété et de défis pour les joueurs. De plus, nous avons travaillé sur l''amélioration de la narration, en ajoutant des dialogues plus riches et des cinématiques qui plongent les joueurs plus profondément dans l''histoire. L''objectif est de créer une expérience narrative captivante et immersive.',
+'01/07/2024',
+NULL),
+('Devlog #8',
+'L''équipe a travaillé sur l''optimisation des performances et la réduction des temps de chargement.',
+'Cette semaine, notre priorité a été l''optimisation des performances du jeu. Nous avons travaillé sur la réduction des temps de chargement et l''amélioration de la fluidité du jeu sur différentes plateformes. Grâce à des techniques de compression avancées et des optimisations du code, nous avons réussi à rendre le jeu plus réactif et agréable à jouer. Ces améliorations devraient permettre à un plus grand nombre de joueurs de profiter pleinement de notre jeu, même sur des configurations matérielles moins puissantes.',
+'08/07/2024',
+NULL),
+('Devlog #9',
+'Nous avons travaillé sur la conception sonore et ajouté de nouvelles musiques et effets sonores.',
+'Cette semaine, notre équipe audio a été très occupée. Nous avons ajouté de nouvelles musiques de fond qui accompagnent les différentes ambiances du jeu, ainsi que des effets sonores immersifs pour les interactions et les combats. Nous avons collaboré avec des compositeurs talentueux pour créer une bande sonore qui s''adapte parfaitement à l''atmosphère du jeu. Les tests initiaux montrent que ces ajouts enrichissent considérablement l''expérience globale du joueur.',
+'15/07/2024',
+NULL),
+('Devlog #10',
+'Cette semaine, nous avons travaillé sur le multijoueur et les fonctionnalités sociales.',
+'Nous avons fait d''énormes progrès cette semaine en intégrant les fonctionnalités multijoueur. Les joueurs peuvent désormais se connecter, former des équipes et jouer ensemble en temps réel. Nous avons également ajouté des fonctionnalités sociales, telles que la possibilité de créer des guildes, de discuter avec d''autres joueurs et de partager des contenus. Ces ajouts visent à renforcer la communauté et à offrir une expérience de jeu plus interactive et connectée.',
+'22/07/2024',
+NULL);
+
+
+
+
+-- Insertion des utilisateurs
+INSERT INTO user (username, mail, password, role)
+VALUES 
+('Marco', 'marco@example.com', 'password_hash_1', 'user'),
+('Matthieu', 'matthieu@example.com', 'password_hash_2', 'user'),
+('Vivien', 'vivien@example.com', 'password_hash_3', 'user'),
+('John', 'john@example.com', 'password_hash_4', 'user');
+
+
+
+-- Insertion des évaluations des jeux par les utilisateurs
+INSERT INTO rates_by_user (user_id, game_id, note)
+VALUES 
+(1, 1, 75),
+(1, 2, 85),
+(1, 3, 60),
+(1, 4, 70),
+(1, 5, 50),
+(1, 6, 65),
+(2, 1, 80),
+(2, 2, 45),
+(2, 3, 90),
+(2, 4, 55),
+(2, 5, 70),
+(2, 6, 60),
+(3, 1, 65),
+(3, 2, 75),
+(3, 3, 50),
+(3, 4, 85),
+(3, 5, 45),
+(3, 6, 70),
+(4, 1, 55),
+(4, 2, 80),
+(4, 3, 60),
+(4, 4, 90),
+(4, 5, 50),
+(4, 6, 65);
