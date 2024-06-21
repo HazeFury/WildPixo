68426f5972b1eb383641f0a2f4b75452f910985c
HazeFury <mab.dev69@gmail.com>
2024-06-21 14:55:59 +0200

    finished a good edit news function. Works fine

--- client/src/pages/News/AddNews/AddNews.jsx
+++ client/src/pages/News/AddNews/AddNews.jsx
@@ -1,16 +1,14 @@
 import { useState, useEffect } from "react";
-import { useLocation, useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import styles from "./AddNews.module.css";
 
 function AddNews() {
   const ApiUrl = import.meta.env.VITE_API_URL;
   const navigate = useNavigate();
-  const location = useLocation();
   const { id } = useParams();
-  const [editMode] = useState(location.pathname.startsWith("/news/edit"));
+  const [editMode] = useState(id !== undefined); // if id is undefined, it means to that you want to create news. If id exist, it means that's this is a news you want to update
   const [actualNews, setActualNews] = useState({});
-  const [isNewsToEdit, setIsNewsToEdit] = useState(false);
   const notifySuccess = (text) => toast.success(text);
   const notifyFail = (text) => toast.error(text);
   const date = new Date();
@@ -24,25 +22,24 @@ function AddNews() {
     id: "",
   });
 
+  // ------------------------- For edit  -----------------------
   useEffect(() => {
+    // execute the following code only if you have and id in order to update the news
     if (editMode === true) {
-      fetch(`${ApiUrl}/news/${id}`)
+      fetch(`${ApiUrl}/news/${id}`) // fetch the news with a given id
         .then((response) => response.json())
         .then((data) => setActualNews(data))
-        .then(() => setIsNewsToEdit(true));
-    }
-  }, [ApiUrl, editMode, id, location.pathname]);
-
-  useEffect(() => {
-    if (isNewsToEdit === true && Object.keys(actualNews).length > 0) {
-      newsForm.title = actualNews.title;
-      newsForm.intro = actualNews.intro;
-      newsForm.content = actualNews.content;
-      newsForm.date = actualNews.date.slice(0, 10);
-      newsForm.id = actualNews.id;
+        .then(() => {
+          newsForm.title = actualNews.title; // replace values from the empty form by values from the news we just got
+          newsForm.intro = actualNews.intro;
+          newsForm.content = actualNews.content;
+          newsForm.date = actualNews.date.slice(0, 10);
+          newsForm.id = actualNews.id;
+        });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isNewsToEdit, actualNews]);
+  }, [ApiUrl, editMode, id, actualNews]);
+  // -----------------------------------------------------------
 
   const handleUpdateForm = (e) => {
     setNewsForm({ ...newsForm, [e.target.name]: e.target.value });
