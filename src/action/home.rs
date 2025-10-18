32f6deda1866086e082693c9933caacbf09c1a9e
spacecodeur <spacecodeur@gmail.com>
2025-10-18 11:34:35 +0200

    Add dynamic templating system with parameterized home page and enhanced HTML meta tags

--- src/action/home.rs
+++ src/action/home.rs
@@ -3,11 +3,19 @@ use askama::Template;
 
 #[derive(Template)]
 #[template(path = "pages/home.html")]
-struct HomeTemplate;
+struct HomeTemplate<'a>{
+    h1: &'a str,
+    title: &'a str,
+    content: &'a str
+}
 
 #[get("")]
 pub async fn homeAction() -> impl Responder {
-    let home_template = HomeTemplate;
+    let home_template = HomeTemplate{
+        title: "titre onglet",
+        h1:"titreh1",  
+        content: "content" 
+    };
     let rendered_template = home_template.render().unwrap();
     HttpResponse::Ok().body(rendered_template)
 }
